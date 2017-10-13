12a7d8cebc446623a04ffa0ce241250649565faa
Filipe Natanael <filipenatanael@live.com>
2017-10-13 17:02:44 -0300

    Project Configuration
    Adjustments have been made and backups

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-import { AppRegistry } from 'react-native';
-import App from './App';
+//Import
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
 
+//Format
+const Estilos = {
+  Texto: {
+    fontSize: 50,
+    backgroundColor: '#3EDE5B',
+    height: 300,
+    //width: 300
+  }
+};
+
+
+//Create component
+const App = () => {
+  return (
+    <Text style={Estilos.Texto}>Olá</Text>
+  );
+}
+//Render to device
 AppRegistry.registerComponent('ReactNativeStarter', () => App);
