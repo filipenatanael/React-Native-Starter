100322807c2029302f063947c5f33fc7bca678cc
Filipe Natanael <filipenatanael@live.com>
2017-10-13 23:47:22 -0300

    Flexbox
    Aligning elements with Flexbox

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ import { Text, View, AppRegistry } from 'react-native';
 
 //Create component
 const App = () => {
-  const { ViewStyle, TextStyle } = Styles;
+  const { ViewStyle, TextStyle1, TextStyle2, } = Styles;
   return (
     <View style={ViewStyle}>
-    <Text style={TextStyle}>Hello World!</Text>
+    <Text style={TextStyle1}>Hello World! 1</Text>
+    <Text style={TextStyle2}>Hello World! 2</Text>
     </View>
   );
 };
@@ -15,9 +16,20 @@ const App = () => {
 
 //Formatting
 const Styles = {
-  TextStyle: {
+  TextStyle1: {
     fontSize: 50,
     backgroundColor: '#99FD77',
+    paddingTop: 20,
+    paddingBottom: 30,
+    margin: 10,
+    marginHorizontal: 10,
+    color: '#000',
+    fontWeight: 'bold', /* Between 100 and 900 */
+    textAlign: 'center',
+  },
+  TextStyle2: {
+    fontSize: 50,
+    backgroundColor: '#D8D8D8',
     paddingTop: 20,
     paddingBottom: 30,
     margin: 10,
@@ -31,6 +43,7 @@ const Styles = {
     height: 300,
     justifyContent: 'center', /* Horizontal alignment */
     /* alignItems: 'center',    Vertical alignment */
+    flexDirection: 'column',
   },
 };
 
