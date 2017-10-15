7a8d7e500890456bab9ee0fcb2ef1c2e6b35e14f
Filipe Natanael <filipenatanael@live.com>
2017-10-15 15:11:45 -0200

    Props And States
    Props And States

--- index.js
+++ index.js
@@ -1,24 +1,33 @@
 import React, { Component } from 'react';
-import { View, Text, AppRegistry } from 'react-native';
+import { View, Button, Text, AppRegistry } from 'react-native';
 
 class MyComponent extends Component {
   render() {
     return (
       <View>
       <Text>{this.props.message}</Text>
-      <Text>My name is {this.props.name}</Text>
+      <Text>{this.props.firstName} {this.props.lastName}</Text>
       </View>
     );
   }
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { stt_firstName: 'Filipe', stt_lastName: 'Natanael', sst_menssage: 'Hello!!' };
+  }
+  changeText() {
+    this.setState({ sst_menssage: 'Hey, how are you?' });
+  }
+
   render() {
     return (
-      <MyComponent
-      name='DeadPool.'
-      message='Hello!!'
-      ></MyComponent>
+      <View>
+      <MyComponent firstName={this.state.stt_firstName} lastName={this.state.stt_lastName} message={this.state.sst_menssage} ></MyComponent>
+
+      <Button title='My Button' onPress={() => { this.changeText() }} />
+      </View>
     );
   }
 }
