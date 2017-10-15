1e928cba35436feacf7ff035ac125fc5abb30303
Filipe Natanael <filipenatanael@live.com>
2017-10-14 23:21:41 -0300

    Button Example
    Button Example

---
+++ Backups/HelloWorld.js
@@ -0,0 +1,63 @@
+//Import
+import React from 'react';
+import { Text, View, AppRegistry } from 'react-native';
+
+//Create component
+const App = () => {
+  const { ViewStyle, TextStyle1, TextStyle2, TextStyle3, } = Styles;
+  return (
+    <View style={ViewStyle}>
+    <Text style={TextStyle1}>Hello World! 1</Text>
+    <Text style={TextStyle2}>Hello World! 2</Text>
+    <Text style={TextStyle3}>Hello World! 3</Text>
+    </View>
+  );
+};
+
+//Formatting
+const Styles = {
+  TextStyle1: {
+    fontSize: 50,
+    backgroundColor: '#99FD77',
+    paddingTop: 20,
+    paddingBottom: 30,
+    margin: 10,
+    marginHorizontal: 10,
+    color: '#000',
+    fontWeight: 'bold', /* Between 100 and 900 */
+    textAlign: 'center',
+  },
+  TextStyle2: {
+    fontSize: 50,
+    backgroundColor: '#D8D8D8',
+    paddingTop: 20,
+    paddingBottom: 30,
+    margin: 10,
+    marginHorizontal: 10,
+    color: '#000',
+    fontWeight: 'bold', /* Between 100 and 900 */
+    textAlign: 'center',
+  },
+  TextStyle3: {
+    fontSize: 50,
+    backgroundColor: '#E2A9F3',
+    paddingTop: 20,
+    paddingBottom: 30,
+    margin: 10,
+    marginHorizontal: 10,
+    color: '#000',
+    fontWeight: 'bold', /* Between 100 and 900 */
+    textAlign: 'center',
+  },
+  ViewStyle: {
+    backgroundColor: '#FFF8DC',
+    height: 500,
+    justifyContent: 'center', /* Horizontal alignment */
+    /* alignItems: 'center',    Vertical alignment */
+    flexDirection: 'column',
+  },
+};
+
+
+//Render to device
+AppRegistry.registerComponent('ReactNativeStarter', () => App);
--- index.js
+++ index.js
@@ -1,54 +1,29 @@
 //Import
 import React from 'react';
-import { Text, View, AppRegistry } from 'react-native';
+import { View, Button, AppRegistry } from 'react-native';
+
+
+const sayHello = () => {
+  alert('Hello');
+}
 
 //Create component
 const App = () => {
-  const { ViewStyle, TextStyle1, TextStyle2, TextStyle3, } = Styles;
+  const { ViewStyle, } = Styles;
   return (
     <View style={ViewStyle}>
-    <Text style={TextStyle1}>Hello World! 1</Text>
-    <Text style={TextStyle2}>Hello World! 2</Text>
-    <Text style={TextStyle3}>Hello World! 3</Text>
+     <Button
+     title='Say Hello'
+     color='#000'
+     accessibilityLabel='Click here to say hello!'
+     onPress={sayHello}
+     />
     </View>
   );
 };
 
 //Formatting
 const Styles = {
-  TextStyle1: {
-    fontSize: 50,
-    backgroundColor: '#99FD77',
-    paddingTop: 20,
-    paddingBottom: 30,
-    margin: 10,
-    marginHorizontal: 10,
-    color: '#000',
-    fontWeight: 'bold', /* Between 100 and 900 */
-    textAlign: 'center',
-  },
-  TextStyle2: {
-    fontSize: 50,
-    backgroundColor: '#D8D8D8',
-    paddingTop: 20,
-    paddingBottom: 30,
-    margin: 10,
-    marginHorizontal: 10,
-    color: '#000',
-    fontWeight: 'bold', /* Between 100 and 900 */
-    textAlign: 'center',
-  },
-  TextStyle3: {
-    fontSize: 50,
-    backgroundColor: '#E2A9F3',
-    paddingTop: 20,
-    paddingBottom: 30,
-    margin: 10,
-    marginHorizontal: 10,
-    color: '#000',
-    fontWeight: 'bold', /* Between 100 and 900 */
-    textAlign: 'center',
-  },
   ViewStyle: {
     backgroundColor: '#FFF8DC',
     height: 500,
